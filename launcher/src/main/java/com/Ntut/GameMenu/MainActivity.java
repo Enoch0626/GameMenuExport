406710a64f2307ade252f377eb6e5cf573749926
Enoch0626 <138033476+Enoch0626@users.noreply.github.com>
2026-01-03 00:17:12 +0800

    Initial commit

---
+++ launcher/src/main/java/com/Ntut/GameMenu/MainActivity.java
@@ -0,0 +1,32 @@
+package com.Ntut.GameMenu;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        var toUnity = findViewById(R.id.btnUnity);
+        toUnity.setOnClickListener(v -> {
+            Intent intent = new Intent(this, com.unity3d.player.UnityPlayerGameActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            startActivity(intent);
+        });
+    }
+}
